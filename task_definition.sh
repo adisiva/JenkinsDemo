c8fe7869c4df0549113cbd9b622c5aa8fa937955
adisiva <61374354+adisiva@users.noreply.github.com>
2020-02-27 16:17:01 +0530

    Update task_definition.sh

--- task_definition.sh
+++ task_definition.sh
@@ -3,7 +3,7 @@
 export get_current_task_id=$(~/.local/bin/aws ecs list-tasks --cluster $cluster_name --desired-status RUNNING --family $task_family | jq -r ".taskArns[0]")
 
 # Create the new revision
-export new_task_revision=$(~/.local/bin/aws ecs register-task-definition --family $task_family --task-role-arn $task_role_arn --network-mode bridge --container-definitions "[{\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"$log_group\",\"awslogs-region\":\"$aws_region\"}},\"name\":\"$container_name\",\"image\":\"$aws_ecr_uri/$image_name:$BUILD_NUMBER\",\"memoryReservation\":$soft_limit,\"portMappings\":[{\"hostPort\":0,\"protocol\":\"tcp\",\"containerPort\":$container_port}],\"essential\":true}]" --cpu $task_cpu --memory $task_memory --tags "[{\"key\":\"ClusterName\",\"value\":\"$cluster_name\"},{\"key\":\"ServiceName\",\"value\":\"$service_name\"}]" | jq --raw-output '.taskDefinition.revision')
+export new_task_revision=$(~/.local/bin/aws ecs register-task-definition --family $task_family --network-mode bridge --container-definitions "[{\"logConfiguration\":{\"logDriver\":\"awslogs\",\"options\":{\"awslogs-group\":\"$log_group\",\"awslogs-region\":\"$aws_region\"}},\"name\":\"$container_name\",\"image\":\"$aws_ecr_uri/$image_name:$BUILD_NUMBER\",\"memoryReservation\":$soft_limit,\"portMappings\":[{\"hostPort\":0,\"protocol\":\"tcp\",\"containerPort\":$container_port}],\"essential\":true}]" --cpu $task_cpu --memory $task_memory --tags "[{\"key\":\"ClusterName\",\"value\":\"$cluster_name\"},{\"key\":\"ServiceName\",\"value\":\"$service_name\"}]" | jq --raw-output '.taskDefinition.revision')
 
 
 #below command stops the running task
